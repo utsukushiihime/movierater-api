10d7bd499bc3d71a243b07a4df348222eda9dc90
creattech <crystalsmcneil@gmail.com>
2020-11-16 19:06:33 -0600

    adds rating and movie views and serializers

--- api/models.py
+++ api/models.py
@@ -6,6 +6,9 @@ class Movie(models.Model):
     title = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
 
+    def __str__(self):
+        return self.title
+
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
@@ -15,3 +18,6 @@ class Rating(models.Model):
     class Meta:
         unique_together = (('user', 'movie'))
         index_together = (('user', 'movie'))
+
+    def __str__(self):
+        return self.movie
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
+from .views import MovieViewSet, RatingViewSet
 
 router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
