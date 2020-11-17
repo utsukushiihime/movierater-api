51a8a1aeb5dea1a0d6c820eeb1a78ff3f06813ac
creattech <crystalsmcneil@gmail.com>
2020-11-16 22:05:40 -0600

    adds create and delete views for ratings

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django.contrib.auth.models import User
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 
@@ -9,15 +10,29 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
         if 'stars' in request.data:
 
             movie = Movie.objects.get(id=pk)
-            print('Movie Title:', movie.title)
+            stars = request.data['stars']
+            # user = request.user
+            user = User.objects.get(id=1)
 
-            response = {'message': 'It works'}
-            return Response(response, status=status.HTTP_200_OK)
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating updated', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message': 'Rating created', 'result': serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+
         else:
             response = {'message': 'You need to provide stars'}
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
