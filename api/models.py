ea02a77c1ca9e437cbe3d6bc25ed8148ea3ddd63
creattech <crystalsmcneil@gmail.com>
2020-11-17 16:12:20 -0600

    adds ratings to model and serializer

--- api/models.py
+++ api/models.py
@@ -6,6 +6,21 @@ class Movie(models.Model):
     title = models.CharField(max_length=32)
     description = models.TextField(max_length=360)
 
+    def no_of_ratings(self):
+        ratings = Rating.objects.filter(movie=self)
+        return len(ratings)
+
+    def avg_rating(self):
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+        for rating in ratings:
+            sum += rating.stars
+
+        if len(ratings) > 0:
+            return sum / len(ratings)
+        else:
+            return 0
+
     def __str__(self):
         return self.title
 
