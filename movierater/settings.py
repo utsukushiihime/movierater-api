40da39ae0245b37d6efb23bf92959753e552724c
creattech <crystalsmcneil@gmail.com>
2020-11-18 22:11:08 -0600

    adds cors-headers

--- movierater/settings.py
+++ movierater/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -89,6 +91,9 @@ REST_FRAMEWORK = {
     }
 }
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:4200"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
